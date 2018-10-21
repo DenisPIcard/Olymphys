a835f17ad8e70ba5330970852e5ffb9d68bac7d8
Denis Picard <denis.picard48@orange.fr>
2018-10-21 19:13:18 +0200

    Commit du 21/10 : intégration de l'interface Juré (écrite par Senem)

--- src/Controller/JuryController.php
+++ src/Controller/JuryController.php
@@ -2,8 +2,17 @@
 // src/Controller/CoreController.php
 namespace App\Controller;
 
+use App\Form\NotesType ;
+use App\Form\PhrasesType ;
+use App\Form\EquipesType ;
+use App\Form\JuresType ;
+
 use App\Entity\Jures ;
 use App\Entity\Equipes ;
+use App\Entity\Totalequipes ;
+use App\Entity\Notes ;
+use App\Entity\Visites ;
+use App\Entity\Phrases ;
 
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -46,28 +55,319 @@ class JuryController extends Controller
 			->getManager()
 			->getRepository('App:Equipes')
 			;
+                $repositoryNotes = $this->getDoctrine()
+		->getManager()
+		->getRepository('App:Notes')
+		;
+
 		$listEquipes=array();
+                $progression=array();
                 
  		foreach ($attrib as $key => $value) 
 		{
 			$equipe=$repositoryEquipes->findOneByLettre($key);
 			$listEquipes[$key] = $equipe;
 			$id = $equipe->getId();
+                        $note=$repositoryNotes->EquipeDejaNotee($id_jure ,$id);
+			$progression[$key] = (!is_null($note)) ? 1 : 0 ;
 
 		}
                 
                 $content = $this->get('templating')->render('cyberjury/accueil.html.twig', 
-			array('listEquipes' => $listEquipes,'jure'=>$jure)
+			array('listEquipes' => $listEquipes,'progression'=>$progression,'jure'=>$jure)
 			);
                 
   
                 
 		return new Response($content);
-        return $this->render('cyberjury/accueil.html.twig');
+
    
         }
+        
+        /**
+	* @Security("has_role('ROLE_JURY')")
+	*/
+	public function infos_equipe(Request $request, Equipes $equipe, $id)
+	{
+		$user=$this->getUser();
+		$nom=$user->getUsername();
 
+		$repositoryJure = $this
+			->getDoctrine()
+			->getManager()
+			->getRepository('App:Jures')
+			;
 
-    
-  }
+		$jure=$repositoryJure->findOneByNomJure($nom);
+		$id_jure = $jure->getId();
 
+		$note=$repositoryNotes = $this->getDoctrine()
+		->getManager()
+		->getRepository('App:Notes')
+		->EquipeDejaNotee($id_jure,$id)
+		;
+		$progression = (!is_null($note)) ? 1 : 0 ;
+
+		$repositoryEquipes = $this
+		->getDoctrine()
+		->getManager()
+		->getRepository('App:Equipes');
+
+		//$equipe=$repositoryEquipes->find($id);
+		$lettre=$equipe->getLettre();
+
+		$repositoryTotEquipes = $this
+		->getDoctrine()
+		->getManager()
+		->getRepository('App:Totalequipes');
+
+		$equipe=$repositoryTotEquipes->findOneByLettreEquipe($lettre);
+		$eq=$repositoryEquipes->findOneByLettre($lettre);
+
+		$repositoryEleves = $this
+		->getDoctrine()
+		->getManager()
+		->getRepository('App:Eleves');
+
+		$listEleves=$repositoryEleves->findByLettreEquipe($lettre);
+
+		$content = $this->get('templating')->render('cyberjury/infos.html.twig',
+			array(
+				'equipe'=>$equipe, 
+				'eq'=>$eq,
+				'listEleves'=>$listEleves, 
+				'id_equipe'=>$id,
+				'progression'=>$progression,
+				'jure'=>$jure
+				)
+			);
+		return new Response($content);   
+        }
+        
+        /**
+	* @Security("has_role('ROLE_JURY')")
+	*/
+  	public function evaluer_une_equipe(Request $request, Equipes $equipe, $id)
+	{
+		$user=$this->getUser();
+		$nom=$user->getUsername();
+
+		$repositoryJure = $this
+			->getDoctrine()
+			->getManager()
+			->getRepository('App:Jures')
+			;
+
+		$jure=$repositoryJure->findOneByNomJure($nom);
+		$id_jure = $jure->getId();
+
+		$repositoryEquipes = $this->getDoctrine()
+		->getManager()
+		->getRepository('App:Equipes');
+		//$equipe = $repositoryEquipes->find($id);
+		$lettre=$equipe->getLettre();
+
+		$repositoryJures = $this->getDoctrine()
+		->getManager()
+		->getRepository('App:Jures');
+		$jure = $repositoryJures->find($id_jure);
+		$attrib = $jure->getAttributions();   
+		
+		$em=$this->getDoctrine()->getManager();
+
+		// Création de l'entité Notes
+		$notes = $repositoryNotes = $this->getDoctrine()
+		->getManager()
+		->getRepository('App:Notes')
+		->EquipeDejaNotee($id_jure, $id);
+
+		$flag=0; 
+
+		if(is_null($notes))
+		{	
+			$notes = new Notes();			
+			$notes->setEquipe($equipe);
+			$notes->setJure($jure);
+			$progression = 0; 
+
+			if($attrib[$lettre]==1)
+			{	
+       			$form = $this->createForm(NotesType::class, $notes, array('EST_PasEncoreNotee'=> true, 'EST_Lecteur'=> true,));
+				$flag=1;
+			}
+			else
+			{
+				$notes->setEcrit(0);
+				// On crée le Formulaire grâce au service form factory.
+       			$form = $this->createForm(NotesType::class, $notes, array('EST_PasEncoreNotee'=> true, 'EST_Lecteur'=> false,));
+			}
+		}
+		else
+		{
+			$notes=$this->getDoctrine()
+			->getManager()
+			->getRepository('App:Notes')
+			->EquipeDejaNotee($id_jure,$id); 
+			$progression = 1; 
+
+			if($attrib[$lettre]==1)
+			{
+       			$form = $this->createForm(NotesType::class, $notes, array('EST_PasEncoreNotee'=> false, 'EST_Lecteur'=> true,));
+				$flag=1;
+			}
+			else
+			{
+			$notes->setEcrit('0');
+       		$form = $this->createForm(NotesType::class, $notes, array('EST_PasEncoreNotee'=> false, 'EST_Lecteur'=> false,));
+			}
+		}
+
+		// Reste de la méthode
+		// Si la requête est en post, c'est que le visiteur a soumis le formulaire. 
+		if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+			// création et gestion du formulaire. 
+
+			$em->persist($notes);
+			$em->flush();
+			$request -> getSession()->getFlashBag()->add('notice', 'Notes bien enregistrées');
+			// puis on redirige vers la page de visualisation de cette note dans le tableau de bord
+			return $this->redirectToroute('cyberjury_tableau_de_bord');
+		}
+		// Si on n'est pas en POST, alors on affiche le formulaire. 
+
+		$content = $this->get('templating')->render('cyberjury/evaluer.html.twig', 
+			array(
+				'equipe'=>$equipe,
+				'form'=>$form->createView(),
+				'flag'=>$flag,
+				'progression'=>$progression,
+				'jure'=>$jure
+				  ));
+		return new Response($content);
+		
+	}
+      
+        /**
+	* @Security("has_role('ROLE_JURY')")
+	*/
+	public function tableau(Request $request)
+	{
+		$user=$this->getUser();
+		$nom=$user->getUsername();
+
+		$repositoryJure = $this
+			->getDoctrine()
+			->getManager()
+			->getRepository('App:Jures')
+			;
+
+		$jure=$repositoryJure->findOneByNomJure($nom);
+		$id_jure = $jure->getId();
+
+		$repository = $this->getDoctrine()
+		->getManager()
+		->getRepository('App:Notes')
+		;
+		$em=$this->getDoctrine()->getManager();
+		// Création de l'entité Notes
+//		$notes = new Notes();
+		$MonClassement = $repository->MonClassement($id_jure);
+		
+		$repository = $this->getDoctrine()
+		->getManager()
+		->getRepository('App:Equipes')
+		;
+		$em=$this->getDoctrine()->getManager();
+
+		$listEquipes = array();
+		$j=1;
+		foreach($MonClassement as $notes)
+		{
+			$id = $notes->getEquipe();
+			$equipe = $repository->find($id);
+			$listEquipes[$j]['id']= $equipe->getId();
+			$listEquipes[$j]['lettre']=$equipe->getLettre();
+			$listEquipes[$j]['titre']=$equipe->getTitreProjet();
+			$listEquipes[$j]['exper']=$notes->getExper();
+			$listEquipes[$j]['demarche']=$notes->getDemarche();
+			$listEquipes[$j]['oral']=$notes->getOral();
+			$listEquipes[$j]['origin']=$notes->getOrigin();
+			$listEquipes[$j]['wgroupe']=$notes->getWgroupe();
+			$listEquipes[$j]['ecrit']=$notes->getEcrit();
+			$listEquipes[$j]['points']=$notes->getPoints();
+			$listEquipes[$j]['urlMemoire']=$equipe->getUrlMemoire();
+			$j++;
+		}
+
+		$content = $this->get('templating')->render('cyberjury/tableau.html.twig', 
+			array('listEquipes'=>$listEquipes,'jure'=>$jure)
+			);
+		return new Response($content);
+	}
+        
+        /**
+	* @Security("has_role('ROLE_JURY')")
+	*/
+	public function phrases(Request $request, Equipes $equipe, $id)
+	{
+
+		$user=$this->getUser();
+		$nom=$user->getUsername();
+
+		$repositoryJure = $this
+			->getDoctrine()
+			->getManager()
+			->getRepository('App:Jures')
+			;
+
+		$jure=$repositoryJure->findOneByNomJure($nom);
+		$id_jure = $jure->getId();
+
+		$notes = $repositoryNotes = $this->getDoctrine()
+		->getManager()
+		->getRepository('App:Notes')
+		->EquipeDejaNotee($id_jure, $id);
+		$progression = (!is_null($notes)) ? 1 : 0 ;
+
+		// Création de l'entité Phrases
+		$repositoryPhrases = $this->getDoctrine()
+		->getManager()
+		->getRepository('App:Phrases');
+
+		$repositoryLiaison = $this->getDoctrine()
+		->getManager()
+		->getRepository('App:Liaison');
+
+		$repositoryEquipes = $this
+		->getDoctrine()
+		->getManager()
+		->getRepository('App:Equipes');
+
+		//$equipe=$repositoryEquipes->find($id);
+
+		$em=$this->getDoctrine()->getManager();
+
+		$form = $this->createForm(EquipesType::class, $equipe, array('Attrib_Phrases'=> true, 'Attrib_Cadeaux'=> false));
+	
+		// Si la requête est en post, c'est que le visiteur a soumis le formulaire. 
+		if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+			// création et gestion du formulaire. 
+			
+			$em->persist($equipe);
+			$em->flush();
+			$request -> getSession()->getFlashBag()->add('notice', 'Phrase et prix amusants bien enregistrés');
+			
+			return $this->redirectToroute('cyberjury_accueil');
+		}
+		// Si on n'est pas en POST, alors on affiche le formulaire. 
+
+		$content = $this->get('templating')->render('App:Jury:phrases.html.twig', 
+			array(
+				'equipe'=>$equipe,
+				'form'=>$form->createView(),
+				'progression'=>$progression,
+				'jure'=>$jure
+				  ));
+		return new Response($content);
+        }    
+}
--- src/Entity/Eleves.php
+++ src/Entity/Eleves.php
@@ -5,87 +5,682 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * Eleves
+ *
+ * @ORM\Table(name="eleves")
  * @ORM\Entity(repositoryClass="App\Repository\ElevesRepository")
  */
 class Eleves
 {
     /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=4)
-     */
-    private $numero_equipe;
+    * @ORM\ManyToOne(targetEntity="App\Entity\Equipes", inversedBy="eleves")
+    * @ORM\JoinColumn(nullable=true)
+    */
+    private $equipeleves;
 
     /**
-     * @ORM\Column(type="string", length=4, nullable=true)
+     * @var int
+     *
+     * @ORM\Column(name="id_eleve", type="integer", nullable=true)
      */
-    private $lettre_national;
+    private $idEleve;
 
     /**
-     * @ORM\Column(type="string", length=63)
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255, nullable=true)
      */
     private $nom;
-
     /**
-     * @ORM\Column(type="string", length=20)
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", length=255, nullable=true)
      */
     private $prenom;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="classe", type="string", length=255, nullable=true)
+     */
+    private $classe;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="sexe", type="string", length=1, nullable=true)
+     */
+    private $sexe;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="numero_adresse", type="string", length=255, nullable=true)
+     */
+    private $numeroAdresse;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nature_voie", type="string", length=255, nullable=true)
+     */
+    private $natureVoie;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="rue", type="string", length=255, nullable=true)
+     */
+    private $rue;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code_postal", type="string", length=255, nullable=true)
+     */
+    private $codePostal;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ville", type="string", length=255, nullable=true)
+     */
+    private $ville;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="complement_adresse", type="string", length=255, nullable=true)
+     */
+    private $complementAdresse;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom_lycee", type="string", length=255, nullable=true)
+     */
+    private $nomLycee;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="denomination_lycee", type="string", length=255, nullable=true)
+     */
+    private $denominationLycee;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="localite_lycee", type="string", length=255, nullable=true)
+     */
+    private $localiteLycee;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="rne_lycee", type="string", length=8, nullable=true)
+     */
+    private $rneLycee;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_equipe", type="smallint", nullable=true)
+     */
+    private $idEquipe;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="numero_equipe", type="smallint", nullable=true)
+     */
+    private $numeroEquipe;
+
+    /**
+    */
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lettre_equipe", type="string",length=1, nullable=true)
+     */
+    private $lettreEquipe;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom_equipe", type="string", length=255, nullable=true)
+     */
+    private $nomEquipe;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
     public function getId()
     {
         return $this->id;
     }
 
-    public function getNumeroEquipe(): ?string
+    /**
+     * Set idEleve
+     *
+     * @param integer $idEleve
+     *
+     * @return Eleves
+     */
+    public function setIdEleve($idEleve)
     {
-        return $this->numero_equipe;
-    }
-
-    public function setNumeroEquipe(string $numero_equipe): self
-    {
-        $this->numero_equipe = $numero_equipe;
+        $this->idEleve = $idEleve;
 
         return $this;
     }
 
-    public function getLettreNational(): ?string
+    /**
+     * Get idEleve
+     *
+     * @return integer
+     */
+    public function getIdEleve()
     {
-        return $this->lettre_national;
+        return $this->idEleve;
     }
 
-    public function setLettreNational(?string $lettre_national): self
-    {
-        $this->lettre_national = $lettre_national;
-
-        return $this;
-    }
-
-    public function getNom(): ?string
-    {
-        return $this->nom;
-    }
-
-    public function setNom(string $nom): self
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Eleves
+     */
+    public function setNom($nom)
     {
         $this->nom = $nom;
 
         return $this;
     }
 
-    public function getPrenom(): ?string
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
     {
-        return $this->prenom;
+        return $this->nom;
     }
 
-    public function setPrenom(string $prenom): self
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return Eleves
+     */
+    public function setPrenom($prenom)
     {
         $this->prenom = $prenom;
 
         return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set classe
+     *
+     * @param string $classe
+     *
+     * @return Eleves
+     */
+    public function setClasse($classe)
+    {
+        $this->classe = $classe;
+
+        return $this;
+    }
+
+    /**
+     * Get classe
+     *
+     * @return string
+     */
+    public function getClasse()
+    {
+        return $this->classe;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Eleves
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set sexe
+     *
+     * @param string $sexe
+     *
+     * @return Eleves
+     */
+    public function setSexe($sexe)
+    {
+        $this->sexe = $sexe;
+
+        return $this;
+    }
+
+    /**
+     * Get sexe
+     *
+     * @return string
+     */
+    public function getSexe()
+    {
+        return $this->sexe;
+    }
+
+    /**
+     * Set numeroAdresse
+     *
+     * @param string $numeroAdresse
+     *
+     * @return Eleves
+     */
+    public function setNumeroAdresse($numeroAdresse)
+    {
+        $this->numeroAdresse = $numeroAdresse;
+
+        return $this;
+    }
+
+    /**
+     * Get numeroAdresse
+     *
+     * @return string
+     */
+    public function getNumeroAdresse()
+    {
+        return $this->numeroAdresse;
+    }
+
+    /**
+     * Set natureVoie
+     *
+     * @param string $natureVoie
+     *
+     * @return Eleves
+     */
+    public function setNatureVoie($natureVoie)
+    {
+        $this->natureVoie = $natureVoie;
+
+        return $this;
+    }
+
+    /**
+     * Get natureVoie
+     *
+     * @return string
+     */
+    public function getNatureVoie()
+    {
+        return $this->natureVoie;
+    }
+
+    /**
+     * Set rue
+     *
+     * @param string $rue
+     *
+     * @return Eleves
+     */
+    public function setRue($rue)
+    {
+        $this->rue = $rue;
+
+        return $this;
+    }
+
+    /**
+     * Get rue
+     *
+     * @return string
+     */
+    public function getRue()
+    {
+        return $this->rue;
+    }
+
+    /**
+     * Set codePostal
+     *
+     * @param integer $codePostal
+     *
+     * @return Eleves
+     */
+    public function setCodePostal($codePostal)
+    {
+        $this->codePostal = $codePostal;
+
+        return $this;
+    }
+
+    /**
+     * Get codePostal
+     *
+     * @return integer
+     */
+    public function getCodePostal()
+    {
+        return $this->codePostal;
+    }
+
+    /**
+     * Set ville
+     *
+     * @param string $ville
+     *
+     * @return Eleves
+     */
+    public function setVille($ville)
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    /**
+     * Get ville
+     *
+     * @return string
+     */
+    public function getVille()
+    {
+        return $this->ville;
+    }
+
+    /**
+     * Set complementAdresse
+     *
+     * @param string $complementAdresse
+     *
+     * @return Eleves
+     */
+    public function setComplementAdresse($complementAdresse)
+    {
+        $this->complementAdresse = $complementAdresse;
+
+        return $this;
+    }
+
+    /**
+     * Get complementAdresse
+     *
+     * @return string
+     */
+    public function getComplementAdresse()
+    {
+        return $this->complementAdresse;
+    }
+
+    /**
+     * Set nomLycee
+     *
+     * @param string $nomLycee
+     *
+     * @return Eleves
+     */
+    public function setNomLycee($nomLycee)
+    {
+        $this->nomLycee = $nomLycee;
+
+        return $this;
+    }
+
+    /**
+     * Get nomLycee
+     *
+     * @return string
+     */
+    public function getNomLycee()
+    {
+        return $this->nomLycee;
+    }
+
+    /**
+     * Set denominationLycee
+     *
+     * @param string $denominationLycee
+     *
+     * @return Eleves
+     */
+    public function setDenominationLycee($denominationLycee)
+    {
+        $this->denominationLycee = $denominationLycee;
+
+        return $this;
+    }
+
+    /**
+     * Get denominationLycee
+     *
+     * @return string
+     */
+    public function getDenominationLycee()
+    {
+        return $this->denominationLycee;
+    }
+
+    /**
+     * Set localiteLycee
+     *
+     * @param string $localiteLycee
+     *
+     * @return Eleves
+     */
+    public function setLocaliteLycee($localiteLycee)
+    {
+        $this->localiteLycee = $localiteLycee;
+
+        return $this;
+    }
+
+    /**
+     * Get localiteLycee
+     *
+     * @return string
+     */
+    public function getLocaliteLycee()
+    {
+        return $this->localiteLycee;
+    }
+
+    /**
+     * Set rneLycee
+     *
+     * @param string $rneLycee
+     *
+     * @return Eleves
+     */
+    public function setRneLycee($rneLycee)
+    {
+        $this->rneLycee = $rneLycee;
+
+        return $this;
+    }
+
+    /**
+     * Get rneLycee
+     *
+     * @return string
+     */
+    public function getRneLycee()
+    {
+        return $this->rneLycee;
+    }
+
+    /**
+     * Set idEquipe
+     *
+     * @param integer $idEquipe
+     *
+     * @return Eleves
+     */
+    public function setIdEquipe($idEquipe)
+    {
+        $this->idEquipe = $idEquipe;
+
+        return $this;
+    }
+
+    /**
+     * Get idEquipe
+     *
+     * @return integer
+     */
+    public function getIdEquipe()
+    {
+        return $this->idEquipe;
+    }
+
+    /**
+     * Set numeroEquipe
+     *
+     * @param integer $numeroEquipe
+     *
+     * @return Eleves
+     */
+    public function setNumeroEquipe($numeroEquipe)
+    {
+        $this->numeroEquipe = $numeroEquipe;
+
+        return $this;
+    }
+
+    /**
+     * Get numeroEquipe
+     *
+     * @return integer
+     */
+    public function getNumeroEquipe()
+    {
+        return $this->numeroEquipe;
+    }
+
+    /**
+     * Set lettreEquipe
+     *
+     * @param string $lettreEquipe
+     *
+     * @return Eleves
+     */
+    public function setLettreEquipe($lettreEquipe)
+    {
+        $this->lettreEquipe = $lettreEquipe;
+
+        return $this;
+    }
+
+    /**
+     * Get lettreEquipe
+     *
+     * @return string
+     */
+    public function getLettreEquipe()
+    {
+        return $this->lettreEquipe;
+    }
+
+    /**
+     * Set nomEquipe
+     *
+     * @param string $nomEquipe
+     *
+     * @return Eleves
+     */
+    public function setNomEquipe($nomEquipe)
+    {
+        $this->nomEquipe = $nomEquipe;
+
+        return $this;
+    }
+
+    /**
+     * Get nomEquipe
+     *
+     * @return string
+     */
+    public function getNomEquipe()
+    {
+        return $this->nomEquipe;
+    }
+
+    /**
+     * Set equipeleves
+     *
+     * @param \Odpf\CyberJuryBundle\Entity\Equipes $equipeleves
+     *
+     * @return Eleves
+     */
+    public function setEquipeleves(\Odpf\CyberJuryBundle\Entity\Equipes $equipeleves = null)
+    {
+        $this->equipeleves = $equipeleves;
+
+        return $this;
+    }
+
+    /**
+     * Get equipeleves
+     *
+     * @return \Odpf\CyberJuryBundle\Entity\Equipes
+     */
+    public function getEquipeleves()
+    {
+        return $this->equipeleves;
     }
 }
